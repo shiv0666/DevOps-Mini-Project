03ee15fc101240e321b0ec4c2de4bbc029048e30
Shivansh lohani <142015021+shiv0666@users.noreply.github.com>
2026-02-03 21:11:29 +0530

    Delete backend directory

--- backend/routes/hotelRoutes.js
+++
@@ -1,9 +0,0 @@
-﻿const express = require("express");
-const router = express.Router();
-const { createHotel, listHotels, seedHotels } = require("../controllers/hotelController");
-
-router.post("/", createHotel);
-router.get("/", listHotels);
-router.get("/seed", seedHotels);
-
-module.exports = router;
--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-﻿const jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  const secret = process.env.JWT_SECRET || "changeme";
-  const expires = process.env.TOKEN_EXPIRES_IN || "7d";
-  return jwt.sign({ id }, secret, { expiresIn: expires });
-};
-
-module.exports = generateToken;
